282023e6bc68c35bec6ac9956865c79a36a3ba5b
sdavid770 <seanasusk46@gmail.com>
2021-04-13 13:27:43 +0930

    Test commit1

--- unit1-solution.sh
+++ unit1-solution.sh
@@ -1,3 +1,4 @@
+#!bin/bash
 mkdir unitexercise1
 cd unitexercise1
 mkdir -m 774 verkaesen
@@ -6,12 +7,17 @@ mkdir -m 003 bepflumheit
 mkdir -m 072 zersinnse
 mkdir -m 316 besprachst
 mkdir -m 500 auswitzer
-mkdir -m
-mkdir -m
-mkdir -m
-mkdir -m
-mkdir -m
-mkdir -m
-mkdir -m
-mkdir
-mkdir
+mkdir -m 400 verrabarbst
+mkdir -m 626 zertritter
+mkdir -m 762 auswitzer/aufgerabarbt
+mkdir -m 002 auswitzer/angewarfen
+mkdir -m 446 auswitzer/angetraust
+mkdir -m 367 zertritter/aufklettheit
+mkdir -m 446 verkaesen/aufwarftest
+mkdir -m 231 auswitzer/angetraust/auspflumse
+mkdir -m 212 zertritter/aufklettheit/angerabarbheit
+mkdir -m 205 verkaesen/aufwarftest/aufgerennung
+mkdir -m 336 auswitzer/aufgerabarbt/befahrt
+mkdir -m 162 auswitzer/aufgerabarbt/einkrauen
+mkdir -m 572 zertritter/aufklettheit/angekrauung
+mkdir -m 002 auswitzer/angewarfen/aufgesetzheit
