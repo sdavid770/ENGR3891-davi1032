d1cb46f2941adf3334710a183021274325b9802b
sdavid770 <seanasusk46@gmail.com>
2021-04-13 22:39:25 +0930

    Done Done

---
+++ unit1-solution2.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+alias m="mkdir -p -m"
+alias sc="sudo chown -c"
+alias o="sudo chmod -c"
+alias cm="chmod -c"
+m 775 unit1exercise2
+cd unit1exercise2
+m 775 verrennkeit
+m 775 enpflumen
+m 775 angetritter
+m 775 besprachtest
+m 775 enpflums
+m 775 verklettt
+m 775 aufgewarfs
+m 775 aushundheit
+m 775 enpflumen/beklettte
+m 775 enpflumen/aufgesitzse
+m 775 angetritter/antrittung
+m 775 verklettt/aufwarfheit
+m 775 angetritter/beschmeckung
+m 775 verklettt/aufwarfheit
+m 775 angetritter/beschmeckung
+m 337 verklettt/aufwarfheit/enwarfst
+m 323 angetritter/beschmeckung/ausgesprachs
+m 420 angetritter/beschmeckung/aufkletts
+m 341 verklettt/aufwarfheit/bestehs
+m 227 enpflumen/aufgesitzse/zerraucher
+m 444 angetritter/antrittung/ankaeser
+m 721 angetritter/beschmeckung/ausrauchheit
+
+o 243 enpflumen/beklettte
+o 173 enpflumen/aufgesitzse
+o 116 angetritter/antrittung
+o 725 verklettt/aufwarfheit
+o 034 angetritter/beschmeckung
+cm 603 verrennkeit
+cm 012 enpflumen
+cm 507 angetritter
+cm 353 besprachtest
+cm 073 enpflums
+cm 711 verklettt
+cm 653 aufgewarfs
+cm 610 aushundheit
+
+sc news:floppy verrennkeit
+sc news:news enpflumen
+sc news:tape angetritter
+sc proxy:fax besprachtest
+sc news:fax enpflums
+sc proxy:tape verklettt
+sc mail:floppy aufgewarfs
+sc news:student aushundheit
+sc mail:student enpflumen/beklettte
+sc news:cdrom enpflumen/aufgesitzse
+sc mail:student angetritter/antrittung
+sc proxy:voice verklettt/aufwarfheit
+sc lp:audio angetritter/beschmeckung
+sc games:tape verklettt/aufwarfheit/enwarfst
+sc news:student angetritter/beschmeckung/ausgesprachs
+sc proxy:floppy angetritter/beschmeckung/aufkletts
+sc news:fax verklettt/aufwarfheit/bestehs
+sc nobody:tape enpflumen/aufgesitzse/zerraucher
+sc games:dip angetritter/antrittung/ankaeser
+sc nobody:cdrom angetritter/beschmeckung/ausrauchheit
+
+
+
+
+
