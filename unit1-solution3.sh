0eb74a2a9c19eb3b1e2d58e0df7992bfbb88ab2c
sdavid770 <seanasusk46@gmail.com>
2021-04-14 00:41:19 +0930

    Done

---
+++ unit1-solution3.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+alias m="mkdir -p -m"
+alias sc="sudo chown -c"
+alias o="sudo chmod -c"
+alias c="chmod -c"
+m 775 unit1exercise3
+cd unit1exercise3
+m 775 anwarftest 
+m 775 einrauchen
+m 775 angetrittst
+m 775 einrabarber
+m 775 aufgetritttete
+m 775 auswitzte
+m 775 zerrabarbt
+m 775 zersetzt
+m 775 aufgetritttete/aufgefahrtete
+m 775 zerrabarbt/angerabarbs
+m 775 zersetzt/ausgekrauen
+m 775 angetrittst/versprachst
+m 775 auswitzte/angeschmecktest
+m 4723 zerrabarbt/angerabarbs/bestehkeit
+m 4274 zerrabarbt/angerabarbs/bekraus
+m 251 aufgetritttete/aufgefahrtete/aufgewitzung
+m 620 angetrittst/versprachst/gepflumse
+m 4024 aufgetritttete/aufgefahrtete/ausgehaltheit
+m 4756 aufgetritttete/aufgefahrtete/aufrennt
+m 732 zerrabarbt/angerabarbs/anfahren
+
+o 4016 aufgetritttete/aufgefahrtete
+o 432 zerrabarbt/angerabarbs
+o 4245 zersetzt/ausgekrauen
+o 4727 angetrittst/versprachst
+o 641 auswitzte/angeschmecktest
+
+c 764 anwarftest 
+c 4650 einrauchen
+c 4354 angetrittst
+c 405 einrabarber
+c 230 aufgetritttete
+c 4071 auswitzte
+c 460 zerrabarbt
+c 4337 zersetzt
+
+sc proxy:dip anwarftest
+sc uucp:proxy einrauchen
+sc proxy:mail angetrittst
+sc student:dip einrabarber
+sc news:mail aufgetritttete
+sc mail:audio auswitzte
+sc uucp:dip zerrabarbt
+sc uucp:dip zersetzt
+sc games:voice aufgetritttete/aufgefahrtete
+sc mail:mail zerrabarbt/angerabarbs
+sc mail:uucp zersetzt/ausgekrauen
+sc mail:proxy angetrittst/versprachst
+sc student:news auswitzte/angeschmecktest
+sc games:student zerrabarbt/angerabarbs/bestehkeit
+sc nobody:floppy zerrabarbt/angerabarbs/bekraus
+sc news:dip aufgetritttete/aufgefahrtete/aufgewitzung
+sc mail:fax angetrittst/versprachst/gepflumse
+sc proxy:news aufgetritttete/aufgefahrtete/ausgehaltheit
+sc news:uucp aufgetritttete/aufgefahrtete/aufrennt
+sc mail:student zerrabarbt/angerabarbs/anfahren
+
