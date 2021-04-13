cd1b5b1c8cb36a453a688c0d09e127c5a3ba3cd7
sdavid770 <seanasusk46@gmail.com>
2021-04-14 01:29:20 +0930

    Done done done

---
+++ unit1-solution4.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+alias m="mkdir -p -m"
+alias sc="sudo chown -c"
+alias o="sudo chmod -c"
+alias c="chmod -c"
+m 775 unit1exercise4
+cd unit1exercise4
+m 775 einwitzse
+m 775 aufwitzung
+m 775 einstehst
+m 775 gelaufkeit
+m 775 verfahrtest
+m 775 verfahrs
+m 775 auffahren
+m 775 begehs
+m 775 einwitzse/befahrtete
+m 775 einwitzse/verklettung
+m 775 auffahren/gekaest
+m 775 verfahrs/bewitzse
+m 775 verfahrtest/angegeher
+m 2736 einwitzse/verklettung/belaufung
+m 603 einwitzse/befahrtete/angesetztest
+m 2542 verfahrtest/angegeher/angehalttete
+m 2376 auffahren/gekaest/zerstehung
+m 2135 auffahren/gekaest/auftrittung
+m 2102 auffahren/gekaest/enrauchkeit
+m 707 einwitzse/befahrtete/aufstehen
+
+o 257 einwitzse/befahrtete
+o 047 einwitzse/verklettung
+o 230 auffahren/gekaest
+o 2657 verfahrs/bewitzse
+o 2761 verfahrtest/angegeher
+
+c 2523 einwitzse
+c 457 aufwitzung
+c 206 einstehst
+c 2650 gelaufkeit
+c 2127 verfahrtest
+c 147 verfahrs
+c 2111 auffahren
+c 320 begehs
+
+sc :voice einwitzse
+sc :proxy aufwitzung
+sc :tape einstehst
+sc :student gelaufkeit
+sc :audio verfahrtest
+sc :dip verfahrs
+sc :news auffahren
+sc :fax begehs
+sc :tape einwitzse/befahrtete
+sc :cdrom einwitzse/verklettung
+sc :student auffahren/gekaest
+sc :voice verfahrs/bewitzse
+sc :cdrom verfahrtest/angegeher
+sc :proxy einwitzse/verklettung/belaufung
+sc :floppy einwitzse/befahrtete/angesetztest
+sc :voice verfahrtest/angegeher/angehalttete
+sc :mail auffahren/gekaest/zerstehung
+sc :tape auffahren/gekaest/auftrittung
+sc :floppy auffahren/gekaest/enrauchkeit
+sc :dip einwitzse/befahrtete/aufstehen
+
