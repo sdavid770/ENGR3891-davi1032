d9fd0bf9719ad08818af471a13fc9216c150d845
sdavid770 <seanasusk46@gmail.com>
2021-04-13 12:46:16 +0930

    Test commit

---
+++ unit1-solution.sh
@@ -0,0 +1,17 @@
+mkdir unitexercise1
+cd unitexercise1
+mkdir -m 774 verkaesen
+mkdir -m 660 enhundst
+mkdir -m 003 bepflumheit
+mkdir -m 072 zersinnse
+mkdir -m 316 besprachst
+mkdir -m 500 auswitzer
+mkdir -m
+mkdir -m
+mkdir -m
+mkdir -m
+mkdir -m
+mkdir -m
+mkdir -m
+mkdir
+mkdir
